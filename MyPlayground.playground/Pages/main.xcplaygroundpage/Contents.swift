eb1cf0d43e48351f5c4059b511cf37db9e5204f6
Marie J Park <hello@mariepublic.com>
2016-08-29 16:33:53 -0400

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,6 +13,11 @@
  */
 // write your code here
 
+func helloWorld() {
+    print ("Hello, world!")
+}
+
+helloWorld()
 
 
 
@@ -22,22 +27,34 @@
 // write your code here
 
 
+func test () {
+    let puppy:String = "Johann"
+    print (puppy)
+}
 
+test()
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
+func greeting(name:String){
+    print ("Hello \(name)!")
+}
 
-
+greeting("Joanna")
+greeting ("Rama")
+greeting ("Elias")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
-
+var something = "Michael"
+something = "Jhantelle"
+greeting(something)
 
 
 /*: question5
@@ -45,8 +62,14 @@
  */
 // write your code here
 
+func newFunction () {
+    var new = "something"
+    print ("something")
+    new = "nothing"
+    print (new)
+}
 
-
-
+newFunction()
+newFunction()
 /*:
 Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions. */
